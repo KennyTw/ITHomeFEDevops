1d19a1e6f853da4ab939aa640f16f1cc38916f18
KennyTW <microbean@gmail.com>
2015-11-13 02:36:29 +0800

    fix travis

--- travis_azure.sh
+++ travis_azure.sh
@@ -3,6 +3,6 @@ export AZURE_STORAGE_ACCOUNT="ithomefedevops"
 export AZURE_STORAGE_ACCESS_KEY="yrRw5W3O9cnF6kBzRGuIUdKeOl9cA6qmfU7RnbTwiqOTIIbY8ADtm5l35F5cVR9QFfcqp0/0Q2+WHJuUnkQjAQ=="
 
 echo "Uploading files..."
-azure storage file upload './app' deploy
+azure storage file upload "./app" deploy
 
 echo "Done"
