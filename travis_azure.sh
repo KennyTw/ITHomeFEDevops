fd11f2c9c7ec1656b7fe96e26937c97e1839bd4f
KennyTW <microbean@gmail.com>
2015-11-14 00:02:19 +0800

    travis azure

--- travis_azure.sh
+++ travis_azure.sh
@@ -2,7 +2,11 @@
 export AZURE_STORAGE_ACCOUNT="ithomefedevops"
 export AZURE_STORAGE_ACCESS_KEY="yrRw5W3O9cnF6kBzRGuIUdKeOl9cA6qmfU7RnbTwiqOTIIbY8ADtm5l35F5cVR9QFfcqp0/0Q2+WHJuUnkQjAQ=="
 
+echo "tar files..."
+tar -zcvf "./source.tgz" "./"
+
+
 echo "Uploading files..."
-azure storage file upload "./" deploy
+azure storage file upload "./source.tgz" deploy
 
 echo "Done"
