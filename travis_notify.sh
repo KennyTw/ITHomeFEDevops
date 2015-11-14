292e4e2a138077e217c5af63cd59e510f451292f
KennyTW <microbean@gmail.com>
2015-11-14 21:56:10 +0800

    web hook

--- travis_notify.sh
+++ travis_notify.sh
@@ -5,8 +5,8 @@ echo "$TRAVIS_BRANCH"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "master deploy"
-  curl --data "" http://54.199.162.1/travis
+  curl --data "" http://ithomefedevops.cloudapp.net:3000/travis_web_hook
 elif [ "$TRAVIS_BRANCH" == "dev" ]; then
   echo "dev deploy"
-  curl --data "" http://54.64.42.1/travis
+  curl --data "" http://ithomefedevops.cloudapp.net:3000/travis_web_hook
 fi
